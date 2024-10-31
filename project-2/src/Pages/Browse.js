4bf759023febae5a00cc1e3fb0520fa2606bcae3
Ian Halstead <ihalstea@iastate.edu>
2024-10-31 12:15:57 -0500

    Added search

--- project-2/src/Pages/Browse.js
+++ project-2/src/Pages/Browse.js
@@ -5,17 +5,34 @@ import BrowseCard from "../Components/BrowseCard";
 function Browse(props) {
   const [menu, setMenu] = props.menuState;
 
+  const [displayedMenu, setDisplayedMenu] = useState(menu);
+
   const [cart, setCart] = props.cartState;
+
+  useEffect(() => {
+    setDisplayedMenu(menu);
+  }, [menu]);
+
+  let searchInput = (e) => {
+    setDisplayedMenu(
+      menu.filter((menuItem) => {
+        return menuItem.name
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase());
+      })
+    );
+  };
 
   return (
     <div className="Browse">
+      <input type="text" onInput={searchInput}></input>
       <p>Browse</p>
       <button onClick={() => props.setCurrentPage("Browse")}>Browse</button>
       <button onClick={() => props.setCurrentPage("Confirmation")}>
         Confirmation
       </button>
       <button onClick={() => props.setCurrentPage("Cart")}>Cart</button>
-      {menu.map((product) => (
+      {displayedMenu.map((product) => (
         <BrowseCard
           cartState={props.cartState}
           key={product.id}
